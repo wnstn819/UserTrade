cb27c1a8606fdfb3ea1483d3a4fbe8935bca139a
wnstn819 <wnstn819@naver.com>
2023-10-18 20:57:09 +0900

    Initial Commit

---
+++ src/main/java/com/example/UsedTrade/UsedTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.UsedTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UsedTrade/UsedTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UsedTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
